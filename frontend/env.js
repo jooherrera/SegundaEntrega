e533e37670655e72eed8b5e0b394e8d3cfa73033
joo <jooherrera4@gmail.com>
2021-09-06 16:22:35 -0300

    Fixed

--- frontend/env.js
+++ frontend/env.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 // const HOST = "https://herrera.glitch.me"
 const HOST = "http://localhost:8080"
 // 
-=======
-//const HOST = "https://herrera.glitch.me"
- const HOST = "http://localhost:8080"
->>>>>>> de5f3d0ce0b2d9f82581ecd2235fa2d9c5f206ac
